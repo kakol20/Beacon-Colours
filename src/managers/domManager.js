a208caea3bf133d5c95d928701eba3f816a4db09
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2024-06-18 17:46:43 +0100

    Read input colour

--- src/managers/domManager.js
+++ src/managers/domManager.js
@@ -1,7 +1,35 @@
 const DOMManager = (function () {
-  return {
-    preload() {
+  let startX = 10;
+  let startY = 10;
 
+  function Generate() {
+    ProcessManager.changeState('setTarget');
+  }
+
+  return {
+    targetColorPicker: 0,
+    
+    generateButton: 0,
+
+    preload() {
+      
     },
+
+    setup() {
+      startX = windowWidth > width ? width + 10 : 10;
+      startY = windowWidth > width ? 10 : height + 10;
+
+      this.targetColorPicker = createColorPicker('rgb(255, 0, 135)');
+      this.targetColorPicker.position(startX, startY);
+      this.targetColorPicker.size(50, 50);
+
+      startY += 50 + 10;
+
+      this.generateButton = createButton('Generate');
+      this.generateButton.position(startX, startY);
+      this.generateButton.mousePressed(Generate);
+
+      startY += this.generateButton.height + 10;
+    }
   }
 })()
--- src/managers/processManager.js
+++ src/managers/processManager.js
@@ -1,9 +1,33 @@
 const ProcessManager = (function () {
+  function DrawColor(lab, size = 25, text = "") {
+    let rgb = OkLab.OkLabtosRGB(lab);
+
+    const x = map(lab.a, ab.min, ab.max, size, width - size);
+    const y = map(lab.b, ab.min, ab.max, size, height - size);
+
+    ellipseMode(CENTER);
+    fill(rgb.P5Color);
+    stroke(strokeCol.P5Color);
+    circle(x, y, size);
+  }
+
   let state = 'nothing';
 
   const maxFPS = 60;
 
+  const ab = {
+    min: -0.32,
+    max: 0.32
+  };
+
   const debugStates = true;
+
+  let targetCol = OkLab.sRGBtoOKLab(new sRGB(255 / 255, 0, 135 / 255));
+
+  let bgCol = OkLab.sRGBtoOKLab(new sRGB(28 / 255, 28 / 255, 28 / 255));
+  bgCol.l *= 1.5;
+
+  const strokeCol = new OkLab(bgCol.l > 0.5 ? 0 : 1, 0, 0);
 
   return {
     changeState(s) {
@@ -13,15 +37,26 @@ const ProcessManager = (function () {
     },
 
     setup() {
-
+      state = 'setTarget';
     },
 
     draw(dt) {
+      background(bgCol.P5Color);
+
+      // DrawColor(targetCol);
+
       switch (state) {
+        case 'setTarget':
+          targetCol = OkLab.sRGBtoOKLab(sRGB.P5ColTosRGB(DOMManager.targetColorPicker.color()));
+
+          this.changeState('nothing');
+          break;
         default:
           // do nothing
           break;
       }
+
+      DrawColor(targetCol);
     }
   }
 })()
