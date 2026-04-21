da54c3781cfbc29c7892789a4507f4b9ccf54d96
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2026-04-21 20:59:30 +0100

    Add DOM input

--- src/sketch.js
+++ src/sketch.js
@@ -1,8 +1,14 @@
-async function setup() {
+let colPicker;
 
+let rgbInput = {
+	'r': {'input': null, 'label': null},
+	'g': {'input': null, 'label': null},
+	'b': {'input': null, 'label': null}
+}
+
+async function setup() {
 	const size = Math.min(windowWidth, windowHeight);
 	createCanvas(size, size);
-
 
 	let font = await loadFont('css/minecraftfont.woff');
 	textFont(font);
@@ -12,17 +18,83 @@ async function setup() {
   textSize(15);
 
 	noLoop();
+
+	let domPos = createVector(
+		windowWidth > windowHeight ? width + 10 : 10, 
+		windowHeight > windowWidth ? height + 10 : 10
+	);
+
+	colPicker = createColorPicker('rgb(199, 68, 64)');
+	colPicker.position(domPos.x, domPos.y);
+	colPicker.size(50, 50);
+	colPicker.input(onPickerChange);
+	domPos.y += 50 + 10;
+
+	rgbInput.r.label = createElement('label', 'R');
+	rgbInput.r.label.position(domPos.x, domPos.y);
+	rgbInput.r.input = createInput('0', 'number');
+	rgbInput.r.input.attribute('min', 0);
+	rgbInput.r.input.attribute('max', 255);
+	rgbInput.r.input.position(domPos.x + rgbInput.r.label.width + 5, domPos.y);
+	rgbInput.r.input.input(onRGBChange);
+	domPos.y += rgbInput.r.input.height + 10;
+
+	rgbInput.g.label = createElement('label', 'G');
+	rgbInput.g.label.position(domPos.x, domPos.y);
+	rgbInput.g.input = createInput('0', 'number');
+	rgbInput.g.input.attribute('min', 0);
+	rgbInput.g.input.attribute('max', 255);
+	rgbInput.g.input.position(domPos.x + rgbInput.g.label.width + 5, domPos.y);
+	rgbInput.g.input.input(onRGBChange);
+	domPos.y += rgbInput.g.input.height + 10;
+
+	rgbInput.b.label = createElement('label', 'B');
+	rgbInput.b.label.position(domPos.x, domPos.y);
+	rgbInput.b.input = createInput('0', 'number');
+	rgbInput.b.input.attribute('min', 0);
+	rgbInput.b.input.attribute('max', 255);
+	rgbInput.b.input.position(domPos.x + rgbInput.b.label.width + 5, domPos.y);
+	rgbInput.b.input.input(onRGBChange);
+	domPos.y += rgbInput.b.input.height + 10;
+
+	onPickerChange();
 }
 
 function draw() {
 	background(28);
+}
 
-	text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lobortis, dolor non aliquam convallis, dui diam condimentum tortor, eu eleifend velit lorem et diam. Quisque pulvinar urna vel nunc accumsan pulvinar. Suspendisse congue sem eu condimentum egestas. Duis at fermentum sapien. Donec eget mattis eros. Sed ultricies magna eget tortor lacinia facilisis. Nulla hendrerit fermentum odio, ut interdum nisl egestas eget. Maecenas quis nisl pharetra, scelerisque urna sit amet, commodo lacus. Nunc et tempus est. Donec auctor sapien at tortor dignissim vestibulum. Nullam pretium ornare massa. Vivamus eu tellus vitae velit fringilla fringilla. Integer accumsan placerat ligula, a consectetur arcu eleifend vitae. Sed quis orci semper, consectetur nisi a, dapibus velit. Morbi non fringilla orci.\n\nPraesent tincidunt nunc nec sollicitudin bibendum. Mauris id lectus lacinia, lobortis velit id, tempor nunc. Aenean eget vehicula magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam elit sapien, rutrum id sem eu, elementum viverra purus. Duis maximus purus sed mi congue tempus. Fusce justo ex, tincidunt nec ligula quis, blandit pharetra leo.', 10, 10, width - 20);
+function onPickerChange() {
+	// console.log(colPicker.value());
 
-	let p = createP('Testing DOM');
-	p.position(
-		windowWidth > windowHeight ? width + 10 : 10, 
-		windowHeight > windowWidth ? height + 10 : 10
-	);
-	p.attribute('align', 'left');
+	const rgb = HexToRGB(colPicker.value());
+
+	rgbInput.r.input.attribute('value', rgb.r);
+	rgbInput.g.input.attribute('value', rgb.g);
+	rgbInput.b.input.attribute('value', rgb.b);
+}
+
+function onRGBChange() {
+	let r = constrain(int(rgbInput.r.input.value()), 0, 255);
+	let g = constrain(int(rgbInput.g.input.value()), 0, 255);
+	let b = constrain(int(rgbInput.b.input.value()), 0, 255);
+
+	colPicker.value(rgbToHex(r, g, b));
+}
+
+function HexToRGB(Hex) {
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(Hex);
+  // return result ? [[parseInt(result[1], 16)], [parseInt(result[2], 16)], [parseInt(result[3], 16)]]: [[]];
+	return result ? {
+		'r': parseInt(result[1], 16),
+		'g': parseInt(result[2], 16),
+		'b': parseInt(result[3], 16)
+	} : {'r': 0, 'g': 0, 'b': 0};
+}
+
+function rgbToHex(r, g, b) {
+  return '#' +
+    hex(r, 2) +
+    hex(g, 2) +
+    hex(b, 2);
 }
