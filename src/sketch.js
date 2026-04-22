3e732c1eac2ef316b81846b3346201ff9c90b192
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2026-04-22 19:12:59 +0100

    Prepare to use modules

--- src/sketch.js
+++ src/sketch.js
@@ -1,100 +1,131 @@
-let colPicker;
+export const DOMs = {
+	colPicker: null,
 
-let rgbInput = {
-	'r': {'input': null, 'label': null},
-	'g': {'input': null, 'label': null},
-	'b': {'input': null, 'label': null}
+  rgbInput: {
+    r: { input: null, label: null },
+    g: { input: null, label: null },
+    b: { input: null, label: null }
+  },
+
+  depthInput: {
+    input: null,
+    label: null
+  },
+
+	button: null
 }
 
-async function setup() {
-	const size = Math.min(windowWidth, windowHeight);
-	createCanvas(size, size);
+const sketch = (p) => {
+	p.setup = async () => {
+		const size = Math.min(p.windowWidth, p.windowHeight);
+		p.createCanvas(size, size);
 
-	let font = await loadFont('css/minecraftfont.woff');
-	textFont(font);
-	textAlign(LEFT, TOP);
-  fill(255);
-  textWrap(WORD);
-  textSize(15);
+		let font = await p.loadFont('css/minecraftfont.woff');
+		p.textFont(font);
+		p.textAlign(p.LEFT, p.TOP);
+		p.fill(255);
+		p.textWrap(p.WORD);
+		p.textSize(15);
 
-	noLoop();
+		p.noLoop();
 
-	let domPos = createVector(
-		windowWidth > windowHeight ? width + 10 : 10, 
-		windowHeight > windowWidth ? height + 10 : 10
-	);
+		let domPos = p.createVector(
+			p.windowWidth > p.windowHeight ? p.width + 10 : 10,
+			p.windowHeight > p.windowWidth ? p.height + 10 : 10
+		);
 
-	colPicker = createColorPicker('rgb(199, 68, 64)');
-	colPicker.position(domPos.x, domPos.y);
-	colPicker.size(50, 50);
-	colPicker.input(onPickerChange);
-	domPos.y += 50 + 10;
+		DOMs.colPicker = p.createColorPicker('rgb(0, 50, 150)');
+		DOMs.colPicker.position(domPos.x, domPos.y);
+		DOMs.colPicker.size(50, 50);
+		DOMs.colPicker.input(onPickerChange);
+		domPos.y += 50 + 10;
 
-	rgbInput.r.label = createElement('label', 'R');
-	rgbInput.r.label.position(domPos.x, domPos.y);
-	rgbInput.r.input = createInput('0', 'number');
-	rgbInput.r.input.attribute('min', 0);
-	rgbInput.r.input.attribute('max', 255);
-	rgbInput.r.input.position(domPos.x + rgbInput.r.label.width + 5, domPos.y);
-	rgbInput.r.input.input(onRGBChange);
-	domPos.y += rgbInput.r.input.height + 10;
+		DOMs.rgbInput.r.label = p.createElement('label', 'R');
+		DOMs.rgbInput.r.label.position(domPos.x, domPos.y);
+		DOMs.rgbInput.r.input = p.createInput('0', 'number');
+		DOMs.rgbInput.r.input.attribute('min', 0);
+		DOMs.rgbInput.r.input.attribute('max', 255);
+		DOMs.rgbInput.r.input.position(domPos.x + DOMs.rgbInput.r.label.width + 5, domPos.y);
+		DOMs.rgbInput.r.input.input(onRGBChange);
+		domPos.y += DOMs.rgbInput.r.input.height + 10;
 
-	rgbInput.g.label = createElement('label', 'G');
-	rgbInput.g.label.position(domPos.x, domPos.y);
-	rgbInput.g.input = createInput('0', 'number');
-	rgbInput.g.input.attribute('min', 0);
-	rgbInput.g.input.attribute('max', 255);
-	rgbInput.g.input.position(domPos.x + rgbInput.g.label.width + 5, domPos.y);
-	rgbInput.g.input.input(onRGBChange);
-	domPos.y += rgbInput.g.input.height + 10;
+		DOMs.rgbInput.g.label = p.createElement('label', 'G');
+		DOMs.rgbInput.g.label.position(domPos.x, domPos.y);
+		DOMs.rgbInput.g.input = p.createInput('0', 'number');
+		DOMs.rgbInput.g.input.attribute('min', 0);
+		DOMs.rgbInput.g.input.attribute('max', 255);
+		DOMs.rgbInput.g.input.position(domPos.x + DOMs.rgbInput.g.label.width + 5, domPos.y);
+		DOMs.rgbInput.g.input.input(onRGBChange);
+		domPos.y += DOMs.rgbInput.g.input.height + 10;
 
-	rgbInput.b.label = createElement('label', 'B');
-	rgbInput.b.label.position(domPos.x, domPos.y);
-	rgbInput.b.input = createInput('0', 'number');
-	rgbInput.b.input.attribute('min', 0);
-	rgbInput.b.input.attribute('max', 255);
-	rgbInput.b.input.position(domPos.x + rgbInput.b.label.width + 5, domPos.y);
-	rgbInput.b.input.input(onRGBChange);
-	domPos.y += rgbInput.b.input.height + 10;
+		DOMs.rgbInput.b.label = p.createElement('label', 'B');
+		DOMs.rgbInput.b.label.position(domPos.x, domPos.y);
+		DOMs.rgbInput.b.input = p.createInput('0', 'number');
+		DOMs.rgbInput.b.input.attribute('min', 0);
+		DOMs.rgbInput.b.input.attribute('max', 255);
+		DOMs.rgbInput.b.input.position(domPos.x + DOMs.rgbInput.b.label.width + 5, domPos.y);
+		DOMs.rgbInput.b.input.input(onRGBChange);
+		domPos.y += DOMs.rgbInput.b.input.height + 10;
 
-	onPickerChange();
-}
+		onPickerChange();
 
-function draw() {
-	background(28);
-}
+		DOMs.depthInput.label = await p.createElement('label', 'Depth');
+		DOMs.depthInput.label.position(domPos.x, domPos.y);
+		DOMs.depthInput.input = p.createInput('6', 'number');
+		DOMs.depthInput.input.attribute('min', 1);
+		DOMs.depthInput.input.attribute('max', 15);
+		DOMs.depthInput.input.position(domPos.x + DOMs.depthInput.label.width + 20, domPos.y);
+		domPos.y += DOMs.depthInput.input.height + 10;
 
-function onPickerChange() {
-	// console.log(colPicker.value());
+		DOMs.button = p.createButton('Calculate');
+		DOMs.button.position(domPos.x, domPos.y);
+		DOMs.button.mousePressed(calculate);
+	};
 
-	const rgb = HexToRGB(colPicker.value());
+	p.draw = () => {
+		p.background(20);
+	};
 
-	rgbInput.r.input.attribute('value', rgb.r);
-	rgbInput.g.input.attribute('value', rgb.g);
-	rgbInput.b.input.attribute('value', rgb.b);
-}
+	function onRGBChange() {
+		let r = p.constrain(p.int(DOMs.rgbInput.r.input.value()), 0, 255);
+		let g = p.constrain(p.int(DOMs.rgbInput.g.input.value()), 0, 255);
+		let b = p.constrain(p.int(DOMs.rgbInput.b.input.value()), 0, 255);
 
-function onRGBChange() {
-	let r = constrain(int(rgbInput.r.input.value()), 0, 255);
-	let g = constrain(int(rgbInput.g.input.value()), 0, 255);
-	let b = constrain(int(rgbInput.b.input.value()), 0, 255);
+		DOMs.colPicker.value(rgbToHex(r, g, b));
+	}
 
-	colPicker.value(rgbToHex(r, g, b));
-}
+	function rgbToHex(r, g, b) {
+		return '#' +
+			p.hex(r, 2) +
+			p.hex(g, 2) +
+			p.hex(b, 2);
+	}
 
-function HexToRGB(Hex) {
-  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(Hex);
-  // return result ? [[parseInt(result[1], 16)], [parseInt(result[2], 16)], [parseInt(result[3], 16)]]: [[]];
-	return result ? {
-		'r': parseInt(result[1], 16),
-		'g': parseInt(result[2], 16),
-		'b': parseInt(result[3], 16)
-	} : {'r': 0, 'g': 0, 'b': 0};
-}
+	function onPickerChange() {
+		// console.log(colPicker.value());
 
-function rgbToHex(r, g, b) {
-  return '#' +
-    hex(r, 2) +
-    hex(g, 2) +
-    hex(b, 2);
-}
+		const rgb = HexToRGB(DOMs.colPicker.value());
+
+		// console.log(rgb);
+
+		DOMs.rgbInput.r.input.value(rgb.r);
+		DOMs.rgbInput.g.input.value(rgb.g);
+		DOMs.rgbInput.b.input.value(rgb.b);
+	}
+
+	function HexToRGB(Hex) {
+		let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(Hex);
+		// return result ? [[parseInt(result[1], 16)], [parseInt(result[2], 16)], [parseInt(result[3], 16)]]: [[]];
+		return result ? {
+			r: parseInt(result[1], 16),
+			g: parseInt(result[2], 16),
+			b: parseInt(result[3], 16)
+		} : { r: 0, g: 0, b: 0 };
+	}
+
+	function calculate() {
+		console.log("Button Pressed");
+	}
+};
+
+new p5(sketch);
